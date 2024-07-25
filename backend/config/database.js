71def8d18d274f0e1690fe45aec257bee756728e
Falguni Shrivastav <99965016+falguni300103@users.noreply.github.com>
2024-07-25 16:49:28 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");//node js library to use mongo db
 
 const connectDatabase=()=>{
-    mongoose.connect(mongodb+srv://falguni300103:mini271017@shoppershub.d3wng.mongodb.net).then((data)=>{//connect() returns promise , .then resolves the promise 
+    mongoose.connect("mongodb+srv://falguni300103:mini271017@shoppershub.d3wng.mongodb.net").then((data)=>{//connect() returns promise , .then resolves the promise 
         console.log(`Mongodb is connected with server :${data.connection.host}`)
     })
 }
