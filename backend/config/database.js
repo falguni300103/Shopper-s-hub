44f4cfcf6281e1c12d811f474a5f2f08603da4b0
Falguni Shrivastav <99965016+falguni300103@users.noreply.github.com>
2024-08-01 14:50:11 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");//node js library to use mongo db
 
 const connectDatabase=()=>{
+    console.log(process.env.DB_URI);
     mongoose.connect(process.env.DB_URI).then((data)=>{//connect() returns promise , .then resolves the promise 
         console.log(`Mongodb is connected with server :${data.connection.host}`)
     })
